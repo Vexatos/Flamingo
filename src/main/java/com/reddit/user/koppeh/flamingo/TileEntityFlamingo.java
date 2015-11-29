3828b0a60e7f3023647d36f3581e2365882bcdb8
asiekierka <asiekierka@gmail.com>
2015-11-29 17:12:45 +0100

    port to Minecraft 1.8.8

--- src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingo.java
+++ src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingo.java
@@ -1,14 +1,14 @@
 package com.reddit.user.koppeh.flamingo;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 
-public class TileEntityFlamingo extends TileEntity {
-
+public class TileEntityFlamingo extends TileEntity implements ITickable {
 	public float wiggle = 0;
 	public float wiggleStrength = 0.0F;
 
 	@Override
-	public void updateEntity() {
+	public void update() {
 		wiggle++;
 		wiggleStrength = Math.max(0.0F, wiggleStrength * 0.9F - 0.1F);
 	}
--- src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingoRenderer.java
+++ src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingoRenderer.java
@@ -1,12 +1,14 @@
 package com.reddit.user.koppeh.flamingo;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
+
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class TileEntityFlamingoRenderer extends TileEntitySpecialRenderer {
@@ -15,18 +17,22 @@ public class TileEntityFlamingoRenderer extends TileEntitySpecialRenderer {
 	private ResourceLocation resource = new ResourceLocation("flamingo", "textures/models/flamingo.png");
 
 	public void renderTileEntityAt(TileEntityFlamingo flamingo, double x, double y, double z, float par8) {
-
 		int rotation = 0;
-		if(flamingo.getWorldObj() != null) {
-			rotation = flamingo.getBlockMetadata() * 360 / 16;
-		}
-
-		float wiggle = (float) Math.sin(flamingo.wiggle + par8) * flamingo.wiggleStrength;
+		float wiggle = 0;
 
 		bindTexture(resource);
 
 		GL11.glPushMatrix();
 		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+
+		if (flamingo != null) {
+			if (flamingo.getWorld() != null) {
+				rotation = flamingo.getBlockMetadata() * 360 / 16;
+			}
+
+			wiggle = (float) Math.sin(flamingo.wiggle + par8) * flamingo.wiggleStrength;
+		}
+
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		GL11.glTranslated(x, y + 2.0, z + 1.0);
 		GL11.glScalef(1.0F, -1.0F, -1.0F);
@@ -39,13 +45,14 @@ public class TileEntityFlamingoRenderer extends TileEntitySpecialRenderer {
 		model.renderAll();
 
 		GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+
 		GL11.glPopMatrix();
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 
 	}
 
 	@Override
-	public void renderTileEntityAt(TileEntity entity, double x, double y, double z, float par8) {
+	public void renderTileEntityAt(TileEntity entity, double x, double y, double z, float par8, int destroyStage) {
 		renderTileEntityAt((TileEntityFlamingo) entity, x, y, z, par8);
 	}
 
