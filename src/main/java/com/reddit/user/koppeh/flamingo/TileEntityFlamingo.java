76e86ddcc3cf0c94d2397b6bdff2ca67d4cb9092
Vexatos <Stuarzt@gmx.de>
2016-05-02 20:51:48 +0200

    Extracted wiggle strength.

--- src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingo.java
+++ src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingo.java
@@ -8,8 +8,8 @@ public class TileEntityFlamingo extends TileEntity implements ITickable {
 	public static final float MAX_WIGGLE_STRENGTH = 40.0F;
 	public static final float DEFAULT_WIGGLE_STRENGTH_PER_CLICK = 15.0F;
 
-	public float wiggle = 0;
-	public float wiggleStrength = 0.0F;
+	float wiggle = 0;
+	float wiggleStrength = 0.0F;
 
 	@Override
 	public void update() {
@@ -25,6 +25,10 @@ public class TileEntityFlamingo extends TileEntity implements ITickable {
 
 	public void wiggle() {
 		wiggleStrength = Math.max(0.0F, Math.min(MAX_WIGGLE_STRENGTH, wiggleStrength + DEFAULT_WIGGLE_STRENGTH_PER_CLICK));
+	}
+
+	public float getWiggleStrength() {
+		return this.wiggleStrength;
 	}
 
 }
