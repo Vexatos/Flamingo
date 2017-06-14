ad56ae82b73a3239e8ffd46619e08a1f3884a3e8
Adrian Siekierka <kontakt@asie.pl>
2017-06-14 20:16:25 +0200

    Port to 1.12.

--- src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingoRenderer.java
+++ src/main/java/com/reddit/user/koppeh/flamingo/TileEntityFlamingoRenderer.java
@@ -14,7 +14,7 @@ public class TileEntityFlamingoRenderer extends TileEntitySpecialRenderer<TileEn
 	private ResourceLocation resource = new ResourceLocation("flamingo", "textures/models/flamingo.png");
 
 	@Override
-	public void renderTileEntityAt(TileEntityFlamingo flamingo, double x, double y, double z, float partialTicks, int destroyStage) {
+	public void renderTileEntityAt(TileEntityFlamingo flamingo, double x, double y, double z, float partialTicks, int destroyStage, float hi) {
 		int rotation = 0;
 		float wiggle = 0;
 
