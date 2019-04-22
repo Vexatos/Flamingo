4ff265dc7b67bfacfa567bbc4963674aa9b5db0e
Prospector <prospectordev@gmail.com>
2019-04-22 12:32:08 -0700

    Update to 1.14pre5

--- src/main/java/com/reddit/user/koppeh/flamingo/FlamingoBlock.java
+++ src/main/java/com/reddit/user/koppeh/flamingo/FlamingoBlock.java
@@ -1,12 +1,13 @@
 package com.reddit.user.koppeh.flamingo;
 
-import net.fabricmc.fabric.block.BreakInteractable;
+import net.fabricmc.fabric.api.block.BlockAttackInteractionAware;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.VerticalEntityPosition;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateFactory;
@@ -18,18 +19,18 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class FlamingoBlock extends Block implements BlockEntityProvider, BreakInteractable {
+public class FlamingoBlock extends Block implements BlockEntityProvider, BlockAttackInteractionAware {
 
 	public static final IntegerProperty ROTATION = IntegerProperty.create("rotation", 0, 15);
-	private static final VoxelShape BOUNDING_SHAPE = Block.createCubeShape(3, 0, 3, 13, 17, 13);
+	private static final VoxelShape OUTLINE_SHAPE = Block.createCuboidShape(3, 0, 3, 13, 17, 13);
 
 	public FlamingoBlock(Settings settings) {
 		super(settings);
 	}
 
 	@Override
-	public VoxelShape getBoundingShape(BlockState state, BlockView world, BlockPos pos) {
-		return BOUNDING_SHAPE;
+	public VoxelShape getOutlineShape(BlockState blockState_1, BlockView blockView_1, BlockPos blockPos_1, VerticalEntityPosition verticalEntityPosition_1) {
+		return OUTLINE_SHAPE;
 	}
 
 	@Override
@@ -55,16 +56,16 @@ public class FlamingoBlock extends Block implements BlockEntityProvider, BreakIn
 	}
 
 	@Override
-	public boolean onBreakInteract(BlockState blockState, World world, BlockPos blockPos, PlayerEntity playerEntity, Hand hand, Direction side) {
-		BlockEntity blockEntity = world.getBlockEntity(blockPos);
+	public boolean onAttackInteraction(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, Direction direction) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
 		if (!world.isClient && blockEntity instanceof FlamingoBlockEntity) {
-			world.addBlockAction(blockPos, this, 0, 0);
+			world.addBlockAction(pos, this, 0, 0);
 		}
 		return false;
 	}
 
 	@Override
-	public boolean onBlockAction(BlockState state, World world, BlockPos pos, int var4, int var5) {
+	public boolean onBlockAction(BlockState state, World world, BlockPos pos, int id, int value) {
 		if (world.getBlockEntity(pos) != null) {
 			((FlamingoBlockEntity) world.getBlockEntity(pos)).wiggle();
 			return true;
