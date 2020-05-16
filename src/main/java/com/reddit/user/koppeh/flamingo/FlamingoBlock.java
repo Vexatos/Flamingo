1d9330e516894652dcc224a7984fba2a09030c11
Yoghurt4C <yoghurt4242@gmail.com>
2020-05-17 02:27:11 +0300

    It compiles on 20w20b

--- src/main/java/com/reddit/user/koppeh/flamingo/FlamingoBlock.java
+++ src/main/java/com/reddit/user/koppeh/flamingo/FlamingoBlock.java
@@ -5,8 +5,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.EntityContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -32,7 +32,7 @@ public class FlamingoBlock extends Block implements BlockEntityProvider, BlockAt
 
 	@Override
 	@Deprecated
-	public VoxelShape getOutlineShape(BlockState blockState_1, BlockView blockView_1, BlockPos blockPos_1, EntityContext context) {
+	public VoxelShape getOutlineShape(BlockState blockState_1, BlockView blockView_1, BlockPos blockPos_1, ShapeContext context) {
 		return OUTLINE_SHAPE;
 	}
 
@@ -63,13 +63,13 @@ public class FlamingoBlock extends Block implements BlockEntityProvider, BlockAt
 	public boolean onAttackInteraction(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, Direction direction) {
 		BlockEntity blockEntity = world.getBlockEntity(pos);
 		if (!world.isClient && blockEntity instanceof FlamingoBlockEntity) {
-			world.addBlockAction(pos, this, 0, 0);
+			world.addSyncedBlockEvent(pos, this, 0, 0);
 		}
 		return false;
 	}
 
 	@Override
-	public boolean onBlockAction(BlockState state, World world, BlockPos pos, int type, int data) {
+	public boolean onSyncedBlockEvent(BlockState state, World world, BlockPos pos, int type, int data) {
 		if (world.getBlockEntity(pos) != null) {
 			((FlamingoBlockEntity) world.getBlockEntity(pos)).wiggle();
 			return true;
